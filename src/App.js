ee9a334dc7c51f74b207f63a0fd3e6dfc004a31b
Pradyut Das <77787607+prxdyut@users.noreply.github.com>
2024-10-26 01:47:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashBoard from './Components/DashBoard'
+
+const App = () => {
+  return (
+    <div>
+      <DashBoard/>
+    </div>
+  )
+}
+
+export default App
